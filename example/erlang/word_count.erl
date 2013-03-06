ba1ec3912c3afe3b23a98498cc140d4d862703bd
Fabian Król <fabian.krol@gmail.com>
2013-03-06 05:45:35 +0100

    #3 print lines + exception

--- example/erlang/word_count.erl
+++ example/erlang/word_count.erl
@@ -1,7 +1,26 @@
 -module(word_count).
 -compile(export_all).
 
+start() ->
+    start("../data").
+
+start(DataPath) ->
+    [ File | _ ] = list_data_dir(DataPath),
+    file(File, DataPath).
+
 list_data_dir(DataPath) ->
     {ok, List} = file:list_dir(DataPath),
     List.
+
+file(Filename, Path) ->
+    File = filename:join(Path, Filename),
+    {ok, Port} = file:open(File, read),
+    print_line(Port),
+    file:close(Port),
+    ok.
+
+print_line(Port) ->
+    {ok, Line} = file:read_line(Port),
+    io:format("~p~n", [Line]),
+    print_line(Port).
 
